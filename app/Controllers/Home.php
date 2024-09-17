631550b544ac84860bccd6105acb1e7e9ef75de5
williblack <40183535+williblack@users.noreply.github.com>
2024-09-16 21:50:17 -0500

    actualizacion 6

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,8 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function index2(): string
+    {
+        return view('welcome_message');
+    }
 }
