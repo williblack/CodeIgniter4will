1ee28b6bddef9555c69a4311f07bb34d54e4c6d9
williblack <40183535+williblack@users.noreply.github.com>
2024-09-16 21:51:35 -0500

    actualizacion 7

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/index2', 'Home::index2');
+$routes->get('index2', 'Home::index2');
 $routes->get('prueba/', 'Prueba::index');
 
